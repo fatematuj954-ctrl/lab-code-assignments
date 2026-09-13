c015379fb83099521eced5ab36af89745b87dfc4
fatematuj954-ctrl <fatematuj954@gmail.com>
2026-09-13 22:49:44 +0600

    Create Insertion sort.c

---
+++ Insertion sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, key, j;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+    printf("Array after sorting:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
